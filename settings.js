a6cfffa53cbe6e2beea44488d5ff78013414d16f
ALVINEBOT1 <126098060+ALVINEBOT1@users.noreply.github.com>
2023-02-22 11:35:55 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94719225807'] //ur owner number
+global.owner = ['94786330116'] //ur owner number
 global.ownername = "ＱＵＥＥＮ-Ｘ ＢＯＴ" //ur owner name
 global.ytname = "CYBER JOHN" //ur yt chanel name
 global.socialm = "ＱＵＥＥＮ-Ｘ ＢＯＴ" //ur github or insta name
@@ -24,11 +24,11 @@ global.location = "India, Mizoram, Aizawl" //ur location
 //bot bomdy 
 //ඔයාලගෙ ස්කැන් කරපුහම එන කොඩ්👇 එක දාන්න
 global.session = "YXF5RWYwZ0I=" //session Id ඔයාලගෙ ස්කැන් කරපුහම එන කොඩ් එක ඔකට දාන්න
-global.ownernomer = "94719225807" //ur number
-global.premium = ['94719225807'] //ur premium number
+global.ownernomer = "94786330116" //ur number
+global.premium = ['94786330116'] //ur premium number
 global.botname = 'QUEEN -X' //ur bot name
-global.linkz = "https://chat.whatsapp.com/Fd5MPEBn7NQ1aviL4V5fa7" //your theme url which will be displayed on whatsapp
-global.websitex = "https://chat.whatsapp.com/Fd5MPEBn7NQ1aviL4V5fa7" //ur website to be displayed
+global.linkz = "https://chat.whatsapp.com/Bpw33A90R0D6ya0SFUSbJS" //your theme url which will be displayed on whatsapp
+global.websitex = "https://chat.whatsapp.com/Bpw33A90R0D6ya0SFUSbJS" //ur website to be displayed
 global.botscript = 'https://github.com/yureshkav/Queen-X' //script link
 global.themeemoji = "🛸" //ur theme emoji
 global.packname = "ＱＵＥＥＮ-Ｘ ＢＯＴ" //ur sticker watermark packname
